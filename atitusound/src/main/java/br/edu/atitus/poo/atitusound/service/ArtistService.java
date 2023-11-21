de163c6e4d261e107f98094abfb4247b08cde3ed
Vitor Quadros <Vitoralgusto452@gmail.com>
2023-11-21 20:28:58 -0300

    AtituSound.master

---
+++ atitusound/src/main/java/br/edu/atitus/poo/atitusound/service/ArtistService.java
@@ -0,0 +1,10 @@
+package br.edu.atitus.poo.atitusound.service;
+
+
+
+import br.edu.atitus.poo.atitusound.entities.ArtistEntity;
+
+public interface ArtistService extends GenericService<ArtistEntity> {
+	
+	
+}
